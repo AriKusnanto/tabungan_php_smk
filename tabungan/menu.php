28a1d857cd99bcb5589dfca99f92ce78567a66dd
AriKusnanto <arikusnanto64@gmail.com>
2019-07-05 19:32:33 +0700

    Add files via upload

---
+++ tabungan/menu.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a></a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="siswa.html">Siswa</a></li>
+			<li><a href="tabungan.html">Cicilan</a></li> 
+		</ul>
+	</div>
+</nav>
